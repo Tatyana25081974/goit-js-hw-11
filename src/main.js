4bc9b5ff3d0abd875aa39f93086ad015a9699f76
Tatyana <tatyanakorabelnikovamama@gmail.com>
2025-01-14 17:55:06 +0200

    add initial files

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import { fetchImages } from './pixabay-api.js';
-import { renderImageCards, clearGallery, showNotification } from './render-functions.js';
+import { fetchImages } from './js/pixabay-api';
+import { renderImageCards, clearGallery, showNotification } from './js/render-functions';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import iziToast from 'izitoast';
